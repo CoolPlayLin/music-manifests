faf11b17fffa97c35507abd35f4af0888d7fc5cc
CoolPlayLin <CoolPlayLin@qq.com>
2024-01-06 14:07:22 +0800

    feat: init submit :sparkles:

---
+++ src/main.js
@@ -0,0 +1,13 @@
+const utilities = require("@coolplaylin/utilities");
+const manifests = require("./config/music.json");
+const fs = require("node:fs");
+
+fs.writeFileSync(
+  "./public/music.json",
+  JSON.stringify(
+    utilities.selectMusic(
+      utilities.generateUniqueCombinations(manifests, 4),
+      872
+    )
+  )
+);
