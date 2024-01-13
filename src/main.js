30adef54ad3a1aea77f177c350590bb23c8cbefe
CoolPlayLin <CoolPlayLin@qq.com>
2024-01-13 20:12:43 +0800

    feat: new :sparkles:

--- src/main.js
+++ src/main.js
@@ -4,10 +4,14 @@ const fs = require("node:fs");
 
 fs.writeFileSync(
   "./public/music.json",
-  JSON.stringify(
-    utilities.selectMusic(
+  JSON.stringify([
+    ...utilities.selectMusic(
       utilities.generateUniqueCombinations(manifests, 4),
       872
-    )
-  )
+    ),
+    ...utilities.selectMusic(
+      utilities.generateUniqueCombinations(manifests, 3),
+      872
+    ),
+  ])
 );
